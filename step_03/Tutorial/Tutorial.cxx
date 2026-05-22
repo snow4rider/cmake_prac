a6160b22018ed71a2b0d7e2cddbfe5131e1a3626
Matthew <matthew.t.mckenney@gmail.com>
2026-05-22 09:14:26 -0600

    finished step3

--- step_03/Tutorial/Tutorial.cxx
+++ step_03/Tutorial/Tutorial.cxx
@@ -1,7 +1,7 @@
 // A simple program that computes the square root of a number
 
 // TODO3: Include <format>
-
+#include <format>
 #include <iostream>
 #include <string>
 
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         // TODO4: Convert the print to use std::format
-        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        std::cout << std::format("Usage: {} number\n", argv[0]);
         return 1;
     }
 
@@ -22,6 +22,5 @@ int main(int argc, char *argv[])
     // calculate square root
     double const outputValue = mathfunctions::sqrt(inputValue);
     // TODO5: Convert the print to use std::format
-    std::cout << "The square root of " << inputValue << " is " << outputValue
-              << std::endl;
+    std::cout << std::format("The square root of {} is {}\n", inputValue, outputValue);
 }
